1cfb454a639b22ea3ec586b3027c699b96a92dd5
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-04 18:31:54 +0600

    update cors policy

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ env.config()
 const PORT = process.env.PORT || 3000;
 const app = express()
 app.use(bodyParser.json())
-app.use(cors())
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://movie-dashboard-delta.vercel.app'], // Allowed domains
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers in incoming requests
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
