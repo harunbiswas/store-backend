d24556568ef859b018c29e7d6cfd2d21fc16d871
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-04 19:56:25 +0600

    update

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
+app.use((req, res, next) => {
+
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
