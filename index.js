050267ad9f6d8a156780d3eb33fbacc599496fb2
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-04 19:54:28 +0600

    update

--- index.js
+++ index.js
@@ -14,18 +14,10 @@ const app = express()
 app.use(bodyParser.json())
 
 
-const allowedOrigins = ['http://localhost:5173', 'https://movie-dashboard-delta.vercel.app'];
+
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: '*', 
 };
 
 app.use(cors(corsOptions));
